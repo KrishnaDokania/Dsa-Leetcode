31f1e729c74901b472e9ea4eec5f8dc772e4b8d0
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-03-17 00:32:32 +0530

    Time: 3 ms (56.26%), Space: 45.2 MB (38.44%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -6,7 +6,7 @@ class Solution {
         sub(nums,0,inner,outer);
         return outer;
     }
-    void sub(int nums[], int index,List<Integer>inner,List<List<Integer>>outer){
+    void sub(int nums[] ,int index,List<Integer>inner, List<List<Integer>>outer){
         if(index==nums.length){
             outer.add(new ArrayList<>(inner));
             return;
@@ -15,7 +15,9 @@ class Solution {
         sub(nums,index+1,inner,outer);
         inner.remove(inner.size()-1);
         index+=1;
-        while(index<nums.length && nums[index]==nums[index-1])index++;
-        sub(nums,index,inner,outer);
-    }
+        while(index<nums.length&&nums[index]==nums[index-1]){
+            index++;
+        }
+            sub(nums,index,inner,outer);
+            }
 }
