6f2f95591dc3b5cfcbbd153ec7c7a3da46422f03
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-04-06 23:56:17 +0530

    Time: 13 ms (51.02%), Space: 117 MB (69.8%) - LeetHub

---
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n, int[] costs) {
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            for (int jump = 1; jump <= 3; jump++) {
+                if (i - jump >= 0) {
+                    dp[i] = Math.min(
+                        dp[i],
+                        dp[i - jump] + costs[i - 1] + jump * jump
+                    );
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
