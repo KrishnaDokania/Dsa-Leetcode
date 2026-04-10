2b443e72ea0b2c0b27ae1cdfd8a11aa0d07ce6eb
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-04-10 19:00:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDifficulty(int[] j, int d) {
+      int n=j.length;
+        if(n<d)return-1;
+        int sum=0;
+        if(n==d){
+            for(int i:j){
+                sum+=i;
+            }
+            return sum;
+        }
+        return solve(0,n,d,j);
+    }
+    int solve(int index,int n,int d,int[]j){
+            int max=j[index];
+        if(d==1){
+            for(int i=index;i<n;i++){
+                max=Math.max(max,j[i]);
+            }
+            return max;
+        }
+        int finalmax=Integer.MAX_VALUE;
+        for(int i=index;i<=n-d;i++){
+            max=Math.max(max,j[i]);
+            int result=max+solve(i+1,n,d-1,j);
+            finalmax=Math.min(finalmax,result);
+        }
+        return finalmax;
+    }
+}
