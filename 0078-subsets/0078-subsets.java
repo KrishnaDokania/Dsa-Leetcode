ee8c805fa3bb3d1c6efd21ea88dbc6e4800156d3
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-06-17 21:51:40 +0530

    Time: 1 ms (88.82%), Space: 44.4 MB (20.34%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-       List<List<Integer>>outer=new ArrayList<>();
-       List<Integer>inner=new ArrayList<>();
-       sub(nums,0,inner,outer);
-       return outer;
-       }
-       void sub(int nums[],int index,List<Integer>inner,List<List<Integer>>outer){
-       if(index>=nums.length){
-       outer.add(new ArrayList<>(inner));
-       return;
-       }
-       inner.add(nums[index]);
-       sub(nums,index+1,inner,outer);
-       inner.remove(inner.size()-1);
-     sub(nums,index+1,inner,outer);
+         List<List<Integer>>result=new ArrayList<>();
+         generate(0,nums,new ArrayList<>(),result);
+         return result;
+    }
+    void generate(int index,int[]num,List<Integer>inner,List<List<Integer>>result){
+        if(index==num.length){
+            result.add(new ArrayList<>(inner));
+            return;
+        }
+        inner.add(num[index]);
+        generate(index+1,num,inner,result);
+        inner.remove(inner.size()-1);
+        generate(index+1,num,inner,result);
 
     }
 }
