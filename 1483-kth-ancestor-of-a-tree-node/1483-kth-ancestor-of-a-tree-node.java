af194d12b3ccd01814e43c09eec76073c9dd940f
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-07-29 01:12:44 +0530

    Time: 77 ms (13.69%), Space: 112.4 MB (62.43%) - LeetHub

---
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.java
@@ -0,0 +1,36 @@
+class TreeAncestor {
+    int row;
+    int col;
+    int ancestor[][];
+    public TreeAncestor(int n, int[] parent) {
+    row=n;
+    col= (int) Math.ceil(Math.log(n) / Math.log(2))+1;
+    ancestor=new int[row][col];
+    for(int[]ances:ancestor)
+    Arrays.fill(ances,-1);
+        for(int i=0;i<n;i++){
+            ancestor[i][0]=parent[i];
+        }
+        for(int j=1;j<col;j++){
+            for(int i=0;i<row;i++){
+                if(ancestor[i][j-1]!=-1)
+                ancestor[i][j]=ancestor[ancestor[i][j-1]][j-1];
+            }
+        }
+    }
+    
+    public int getKthAncestor(int node, int k) {
+        for(int i=0;i<col;i++){
+            if((k&(1<<i))!=0)
+            node=ancestor[node][i];
+            if(node==-1)return -1;
+        }
+        return node;
+    }
+}
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor obj = new TreeAncestor(n, parent);
+ * int param_1 = obj.getKthAncestor(node,k);
+ */
