113b13818d6eca69ad9f3aa845390470882ee463
KrishnaDokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-07-30 07:08:29 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n=word.length();
+        int i=1;
+        int sum=0;
+        while(n>=8){
+            sum+=8*i;
+            n=n-8;
+            i++;
+        }
+        if(n>0)sum+=n*i;
+        return sum;
+    }
+}
