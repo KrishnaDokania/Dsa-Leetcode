c832084c586f3f1ab233ca8aac2bcbf1fcb27a64
Krishna Dokania <147261200+KrishnaDokania@users.noreply.github.com>
2026-08-06 01:52:20 +0530

    Time: 64 ms (72.73%), Space: 288.2 MB (47.27%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,53 +1,45 @@
 class Solution {
-
+    boolean check[];
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
-
-        List<List<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
+        List<List<Integer>>adj=new ArrayList<>();
+        check=new boolean[n];
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-
-        for (int[] edge : invocations) {
-            adj.get(edge[0]).add(edge[1]);
+        for(int[]i:invocations){
+            int u=i[0];
+            int v=i[1];
+            adj.get(u).add(v);
         }
-
-        boolean[] suspicious = new boolean[n];
-
-        dfs(k, adj, suspicious);
-
-        // If any non-suspicious method calls a suspicious one,
-        // we cannot remove anything.
-        for (int[] edge : invocations) {
+        dfs(k,adj);
+               for (int[] edge : invocations) {
             int u = edge[0];
             int v = edge[1];
 
-            if (!suspicious[u] && suspicious[v]) {
+            if (!check[u] && check[v]) {
                 List<Integer> ans = new ArrayList<>();
-                for (int i = 0; i < n; i++)
+                for (int i = 0; i < n; i++) {
                     ans.add(i);
+                }
                 return ans;
             }
         }
 
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            if (!suspicious[i])
-                ans.add(i);
-        }
-
-        return ans;
-    }
-
-    void dfs(int node, List<List<Integer>> adj, boolean[] suspicious) {
-
-        suspicious[node] = true;
-
-        for (int neigh : adj.get(node)) {
-            if (!suspicious[neigh]) {
-                dfs(neigh, adj, suspicious);
+            if (!check[i]) {
+                result.add(i);
             }
         }
+
+        return result;
+    }
+    void dfs(int root,List<List<Integer>>adj){
+        if(check[root]==true)return;
+        check[root]=true;
+        for(int neigh:adj.get(root)){
+            dfs(neigh,adj);    
+            }
     }
 }
